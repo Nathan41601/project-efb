dcab701a1c4ee557c0717b22ac5994d10c480b60
Keneth Murillo <keneth.murillo@outlook.com>
2022-11-22 19:59:37 -0600

    Database Project and Database Scripts

---
+++ EnhancerForBusiness/data/EnhancerForBusiness-Database/dbo/Tables/CartItems.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[CartItems] (
+    [CartItemId]  INT            IDENTITY (1, 1) NOT NULL,
+    [CartId]      NVARCHAR (MAX) NOT NULL,
+    [ProductId]   INT            NOT NULL,
+    [Count]       INT            NOT NULL,
+    [DateCreated] DATETIME       NOT NULL,
+    CONSTRAINT [PK_dbo.CartItems] PRIMARY KEY CLUSTERED ([CartItemId] ASC),
+    CONSTRAINT [FK_dbo.CartItems_dbo.Products_ProductId] FOREIGN KEY ([ProductId]) REFERENCES [dbo].[Products] ([ProductId]) ON DELETE CASCADE
+);
+
