30273c174f601d6473cd00db777ad27393497d57
Keneth Murillo <keneth.murillo@outlook.com>
2022-11-22 18:22:57 -0600

    rebuild project

---
+++ EnhancerForBusiness/src/EnhancerForBusiness-Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EnhancerForBusiness_Web.MvcApplication" Language="C#" %>
---
+++ EnhancerForBusiness/tests/EnhancerForBusiness-Web.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EnhancerForBusiness-Web.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EnhancerForBusiness-Web.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8d05baef-98a3-4e2f-9c14-c0a28548d933")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
