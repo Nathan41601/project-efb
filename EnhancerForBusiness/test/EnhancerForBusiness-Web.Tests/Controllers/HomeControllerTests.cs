a3a0f63f568f0f9de19193b7e729dc1808ef5b39
Keneth Murillo <keneth.murillo@outlook.com>
2022-11-23 00:31:59 -0600

    Web Tests

---
+++ EnhancerForBusiness/test/EnhancerForBusiness-Web.Tests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using EnhancerForBusiness_Web.Controllers;
+using EnhancerForBusiness_Web.Tests.Mocks;
+using EnhancerForBusiness_Web.ViewModels;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EnhancerForBusiness_Web.Tests.Controllers
+{
+    [TestClass]
+    public class HomeControllerTests
+    {
+        [TestMethod]
+        public void Home_Index()
+        {
+            // arrange
+            var controller = new HomeController(new MockDataContext());
+
+            // act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // assert
+            Assert.IsNotNull(result);
+            var model = result.Model as HomeViewModel;
+            Assert.IsNotNull(model);
+            Assert.AreEqual(4, model.TopSellingProducts.Count);
+            Assert.IsTrue(model.TopSellingProducts.Any(t => t.ProductId == 1));
+            Assert.IsTrue(model.TopSellingProducts.Any(t => t.ProductId == 2));
+            Assert.IsTrue(model.TopSellingProducts.Any(t => t.ProductId == 3));
+            Assert.IsTrue(model.TopSellingProducts.Any(t => t.ProductId == 4));
+            Assert.IsFalse(model.TopSellingProducts.Any(t => t.ProductId == 5));
+            Assert.IsFalse(model.TopSellingProducts.Any(t => t.ProductId == 6));
+            Assert.AreEqual(4, model.NewProducts.Count);
+            Assert.AreEqual(4, model.CommunityPosts.Count);
+        }
+    }
+}
---
+++ EnhancerForBusiness/test/EnhancerForBusiness-Web.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EnhancerForBusiness-Web.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EnhancerForBusiness-Web.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("707028f2-d11f-4ae6-9fbb-411c13575d74")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
